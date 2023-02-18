1d3b7697d6c8527d68624fd3736b709a2c2197b0
BattlerHaru <kronk.sdance@gmail.com>
2023-02-18 02:18:20 -0600

    promises chaining

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -26,13 +26,14 @@ export const promisesComponent = (element) => {
   const id1 = "5d86371fd55e2e2a30fe1cc4";
   const id2 = "5d86371fd55e2e2a30fe1ccb2";
 
+  let hero1;
   findHero(id1)
-    .then((hero1) => {
-      findHero(id2)
-        .then((hero2) => {
-          renderTwoHeroes(hero1, hero2);
-        })
-        .catch(renderError);
+    .then((hero) => {
+      hero1 = hero;
+      return findHero(id2);
+    })
+    .then((hero2) => {
+      renderTwoHeroes(hero1, hero2);
     })
     .catch(renderError);
 };

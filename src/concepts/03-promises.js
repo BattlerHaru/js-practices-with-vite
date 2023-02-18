90eb9d7fcf2f52c69b884778b5256b8caa8041a8
BattlerHaru <kronk.sdance@gmail.com>
2023-02-18 02:06:13 -0600

    promises

---
+++ src/concepts/03-promises.js
@@ -0,0 +1,40 @@
+import {heroes} from "../data/heroes";
+
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const promisesComponent = (element) => {
+  const renderHero = (hero) => {
+    element.innerHTML = hero.name;
+  };
+
+  const renderError = (error) => {
+    element.innerHTML = `
+     <h1>Error:</h1>
+     <h3>${error}</h3>
+     `;
+  };
+
+  const id1 = "5d86371fd55e2e2a30fe1cc4";
+  const id2 = "5d86371fd55e2e2a30fe1ccb2";
+
+  findHero(id1).then(renderHero).catch(renderError);
+};
+
+/**
+ *
+ * @param {String} id
+ * @returns {Promise<String>}
+ */
+const findHero = (id) => {
+  return new Promise((resolve, reject) => {
+    const hero = heroes.find((hero) => hero.id === id);
+    if (hero) {
+      resolve(hero);
+      return;
+    }
+
+    reject(`Hero with id $${id} not found`);
+  });
+};

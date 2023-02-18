53cfdecc0118397a8362e7c9695294fe89887019
BattlerHaru <kronk.sdance@gmail.com>
2023-02-18 02:25:47 -0600

    promise all

--- src/concepts/03-promises.js
+++ src/concepts/03-promises.js
@@ -27,12 +27,9 @@ export const promisesComponent = (element) => {
   const id2 = "5d86371fd55e2e2a30fe1ccb2";
 
   let hero1;
-  findHero(id1)
-    .then((hero) => {
-      hero1 = hero;
-      return findHero(id2);
-    })
-    .then((hero2) => {
+
+  Promise.all([findHero(id1), findHero(id2)])
+    .then(([hero1, hero2]) => {
       renderTwoHeroes(hero1, hero2);
     })
     .catch(renderError);
